497841b261f0052c9eceb22c8d5425d05633a80b
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-11-26 11:29:12 +0700

    navbar #2

---
+++ client/shopping/src/components/navbar/Announcement.jsx
@@ -0,0 +1,13 @@
+import './announcement.css'
+
+import React from 'react'
+
+const Announcement = () => {
+  return (
+    <div className="an_container">
+        Super Deal! Free Shipping on Orders Over $50
+    </div>
+  )
+}
+
+export default Announcement
--- client/shopping/src/components/navbar/Navbar.jsx
+++ client/shopping/src/components/navbar/Navbar.jsx
@@ -1,20 +1,38 @@
+import { Badge } from '@material-ui/core'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
+import Announcement from './Announcement'
 import "./navbar.css"
 
 const Navbar = () => {
   return (
-    <div className='nb_container'>
-      <div className="nb_wrapper">
-        <div className="nb_left">
-          <div className="nb_language">EN</div>
-          <div className="nb_search">
-            
+    <>
+      <Announcement/>
+      <div className='nb_container'>
+        <div className="nb_wrapper">
+          <div className="nb_left">
+            <div className="nb_language">EN</div>
+            <div className="nb_search">
+              <input type="text" className="nb_input" />
+              <Search style={{color:'gray',fontSize:16}}/>
+            </div>
+          </div>
+          <div className="nb_center">
+            <h1>77Shop</h1>
+          </div>
+          <div className="nb_right">
+  
+            <div className="nb_menuItem">REGISTER</div>
+            <div className="nb_menuItem">SIGN IN</div>
+            <div className="nb_menuItem">
+              <Badge badgeContent={3} color="primary">
+                <ShoppingCartOutlined/>
+              </Badge>
+            </div>
           </div>
         </div>
-        <div className="nb_center">center</div>
-        <div className="nb_right">right</div>
       </div>
-    </div>
+    </>
   )
 }
 
