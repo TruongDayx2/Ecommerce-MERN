c362acb831f8b25a24e3612f5a1e1957f2ba418a
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-03 00:32:09 +0700

    slide #2

--- client/shopping/src/components/slider/Slider.jsx
+++ client/shopping/src/components/slider/Slider.jsx
@@ -1,28 +1,47 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
-import React from 'react'
-import './slider.css'
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import React from "react";
+import { useState } from "react";
+import "./slider.css";
+import { sliderItems } from "../../data";
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {};
+
   return (
-    <div className='sl_container'>
-        <div className="sl_arrow sl_left">
-            <ArrowLeftOutlined/>
-        </div>
-        <div className="sl_wrapper">
-          <div className='sl_slide'>
+    <div className="sl_container">
+      <div className="sl_arrow sl_left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </div>
+      <div className="sl_wrapper">
+        {sliderItems.map((item) => (
+          <div className="sl_slide">
             <div className="sl_img">
-              {/* <img src="https://ssstutter.com/img/lookbook/1.jpg" alt="" className="sl_img__item" /> */}
+              <img
+                src="https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1667804871800.jpeg"
+                alt=""
+                className="sl_img__item"
+              />
+              <img
+                src="https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1667904482646.jpeg"
+                alt=""
+                className="sl_img__item"
+              />
             </div>
             <div className="sl_info">
+              <h1 className="sl_title">FALL into SWEATER</h1>
+              <p className="sl_desc">Immerse Yourself in The Midst Of Nature</p>
+              <button className="sl_btn">SHOW NOW</button>
+            </div>
           </div>
-
-          </div>
-        </div>
-        <div className="sl_arrow sl_right">
-            <ArrowRightOutlined/>
-        </div>
+        ))}
+      </div>
+      <div className="sl_arrow sl_right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
