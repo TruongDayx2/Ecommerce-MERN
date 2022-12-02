ec794907ef36b73247ad754a7e991434780f6549
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-03 00:32:31 +0700

    SLide #2

---
+++ client/shopping/src/data.js
@@ -0,0 +1,16 @@
+export const sliderItems = [
+    {
+        id:1,
+        img1:"https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1667804871800.jpeg",
+        img2:"https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1667904482646.jpeg",
+        title:"FALL into SWEATER",
+        desc:"Immerse Yourself in The Midst Of Nature",
+    },
+    {
+        id:2,
+        img1:"https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/102022/1666162324680.jpeg",
+        img2:"https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/102022/1666556378017.jpeg",
+        title:"FALL into BLAZERS",
+        desc:"Spruce Up Your Everyday Luxury Look",
+    }
+]
