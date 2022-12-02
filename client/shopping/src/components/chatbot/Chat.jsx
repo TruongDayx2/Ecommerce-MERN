62d157f51d49a490881c8834c780e521ecf8e1dc
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-02 16:13:26 +0700

    chatbot #3

--- client/shopping/src/components/chatbot/Chat.jsx
+++ client/shopping/src/components/chatbot/Chat.jsx
@@ -1,13 +1,14 @@
-import { RemoveOutlined } from "@material-ui/icons";
+import { ForumOutlined, RemoveOutlined, SendOutlined } from "@material-ui/icons";
 import { useState, useEffect, useRef } from "react";
 import "./Chat.css";
 import Message from "./Message";
 
-const mes = []
+const mes = [];
 
 const Chat = ({ user }) => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
+  const [turnChat, setTurnChat] = useState(false);
   const scrollRef = useRef();
 
   useEffect(() => {
@@ -23,53 +24,65 @@ const Chat = ({ user }) => {
   const sendMessages = (e) => {
     e.preventDefault();
 
-    if (input !== "") {
+    if (input !== "" && input.trim() !=="") {
       const newMessage = {
-        message:input,
-        role:'You',
-      }
+        message: input,
+        role: "You",
+      };
 
-      mes.push(newMessage)
+      mes.push(newMessage);
       setInput("");
       scrollRef.current.scrollIntoView({ behavior: "smooth" });
-      console.log(mes)
+      console.log(mes);
     }
   };
 
+  console.log(turnChat);
   return (
-    <div className="chat__container">
-      <div className="chat__box">
-        <div className="chat__header">
-          <div className="chat__header__left">
-            <span>Welcome to 77Shop!</span>
-            <p>Can I help you?</p>
+    <>
+      {!turnChat && (
+        <div className="chat__icon" onClick={() => setTurnChat(!turnChat)}>
+          <ForumOutlined style={{ fontSize: "50px" }} />
+        </div>
+      )}
+      {turnChat && (
+        <div className="chat__container">
+          <div className="chat__box">
+            <div className="chat__header">
+              <div className="chat__header__left">
+                <span>Welcome to 77Shop!</span>
+                <p>Can I help you?</p>
+              </div>
+              <div className="chat__header__right" onClick={() => setTurnChat(!turnChat)}>
+                <RemoveOutlined />
+              </div>
+            </div>
+            <div className="chat__messages">
+              {mes.map((message) => (
+                <Message key={message.id} message={message} />
+              ))}
+              <div
+                ref={scrollRef}
+                style={{ float: "left", clear: "both", paddingTop: "4rem" }}
+              ></div>
+            </div>
+            <div className="chat__input">
+              <form onSubmit={sendMessages} style={{ width: "375px",display:"flex",alignItems:"center" }}>
+                <input
+                  type="text"
+                  placeholder="Type a message here"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                />
+                <button>
+                  <SendOutlined/>
+                </button>
+              </form>
+            </div>
           </div>
-          <div className="chat__header__right">
-            <RemoveOutlined/>
-          </div>
         </div>
-        <div className="chat__messages">
-          {mes.map((message) => (
-            <Message key={message.id} message={message} />
-          ))}
-          <div
-            ref={scrollRef}
-            style={{ float: "left", clear: "both", paddingTop: "4rem" }}
-          ></div>
-        </div>
-        <div className="chat__input">
-          <form onSubmit={sendMessages} style={{width: "375px"}}>
-            <input
-              type="text"
-              placeholder="Type a message here"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <button>&rarr;</button>
-          </form>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
