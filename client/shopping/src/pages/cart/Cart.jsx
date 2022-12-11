4a323087c60effff691a94325def9e480f81ae24
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-12 01:08:25 +0700

    cart page #2 Design

--- client/shopping/src/pages/cart/Cart.jsx
+++ client/shopping/src/pages/cart/Cart.jsx
@@ -50,8 +50,57 @@ const Cart = () => {
                 <div className="cart_proPrice">$ 30</div>
               </div>
             </div>
+            <hr className="cart_hr"></hr>
+            <div className="cart_product">
+              <div className="cart_productDetail">
+                <img
+                  src="https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1668409522619.jpeg"
+                  alt=""
+                  className="cart_proImg"
+                />
+                <div className="cart_proDetail">
+                  <span className="cart_proName">
+                    <b>Product:</b> LONG SLEEVED SHIRT
+                  </span>
+                  <span className="cart_proId">
+                    <b>Id:</b> 8391037456
+                  </span>
+                  <div className="cart_proColor" style={{ backgroundColor: "red" }}></div>
+                  <span className="cart_proSize">
+                    <b>Size:</b> L
+                  </span>
+                </div>
+              </div>
+              <div className="cart_priceDetail">
+                <div className="cart_proAmountContainer">
+                  <Add/>
+                  <div className="cart_proAmount">2</div>
+                  <Remove/>
+                </div>
+                <div className="cart_proPrice">$ 30</div>
+              </div>
+            </div>
           </div>
-          <div className="cart_summary">summary</div>
+          <div className="cart_summary">
+            <h1 className="cart_summaryTitle">ORDER SUMMARY</h1>
+            <div className="cart_summaryItem">
+              <span className="cart_summaryItem_Text">Subtotal</span>
+              <span className="cart_summaryItem_Price">$ 60</span>
+            </div>
+            <div className="cart_summaryItem">
+              <span className="cart_summaryItem_Text">Estimated Shipping</span>
+              <span className="cart_summaryItem_Price">$ 5</span>
+            </div>
+            <div className="cart_summaryItem">
+              <span className="cart_summaryItem_Text">Shipping Discount</span>
+              <span className="cart_summaryItem_Price">$ -5</span>
+            </div>
+            <div className="cart_summaryItem" style={{fontWeight:'500', fontSize:'24px'}}>
+              <span className="cart_summaryItem_Text">Total</span>
+              <span className="cart_summaryItem_Price">$ 60</span>
+            </div>
+            <button className="cart_summaryBtn">CHECKOUT NOW</button>
+          </div>
         </div>
       </div>
       <Footer />
