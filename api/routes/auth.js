f7063a9f4d308c6a951bb803f91544ebe465af79
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-19 20:49:07 +0700

    CRUD User

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,35 +1,42 @@
 const router = require("express").Router()
 const User = require('../models/User')
 const CryptoJS = require("crypto-js")
+const jwt = require('jsonwebtoken')
 
 //REGISTER
-router.post('/register', async (req,res)=>{
+router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password:CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
-    }) 
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    })
 
-    try{
+    try {
         const savedUser = await newUser.save()
         res.status(201).json(savedUser)
-    }catch(e){
+    } catch (e) {
         res.status(500).json(e)
     }
 })
 
 //LOGIN
-router.post('/login', async(req,res)=>{
+router.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({username:req.body.username})
+        const user = await User.findOne({ username: req.body.username })
         !user && res.status(401).json('wrong credentials!')
 
         const hashedPass = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
         const pass = hashedPass.toString(CryptoJS.enc.Utf8)
         pass !== req.body.password && res.status(401).json('wrong credentials!')
 
-        const { password, ...others} = user._doc
-        res.status(200).json(others)
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_SEC, { expiresIn: '3d' }
+        )
+
+        const { password, ...others } = user._doc
+        res.status(200).json({ ...others, accessToken })
     } catch (e) {
         res.status(500).json(e)
     }
