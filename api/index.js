bec455244114b90907ac7e166d3ceb33e1b593c6
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-19 15:19:46 +0700

    Authentication: Login API & Register API

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
+const authRoute = require('./routes/auth')
 
 dotenv.config()
 
 app.use(express.json())
+app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 
 
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,38 @@
 const router = require("express").Router()
+const User = require('../models/User')
+const CryptoJS = require("crypto-js")
 
+//REGISTER
+router.post('/register', async (req,res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password:CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    }) 
 
+    try{
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    }catch(e){
+        res.status(500).json(e)
+    }
+})
+
+//LOGIN
+router.post('/login', async(req,res)=>{
+    try {
+        const user = await User.findOne({username:req.body.username})
+        !user && res.status(401).json('wrong credentials!')
+
+        const hashedPass = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
+        const pass = hashedPass.toString(CryptoJS.enc.Utf8)
+        pass !== req.body.password && res.status(401).json('wrong credentials!')
+
+        const { password, ...others} = user._doc
+        res.status(200).json(others)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
 
 module.exports = router
