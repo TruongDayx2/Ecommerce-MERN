3dbc91608a32e516c65b558be450e99c971c1697
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-19 21:37:23 +0700

    CURD Product

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
 
 dotenv.config()
 
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/products", productRoute)
 
 
 mongoose
--- api/routes/user.js
+++ api/routes/user.js
@@ -4,81 +4,81 @@ const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = requir
 const router = require("express").Router()
 
 // UPDATE
-router.put("/:id",verifyTokenAndAuthorization,async(req,res)=>{
-    if(req.body.password){
-        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
-    }
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+  }
 
-    try {
-        const updateUser = await User.findByIdAndUpdate(req.params.id ,{
-            $set: req.body
-        },{new:true})
-        res.status(200).json(updateUser)
-    } catch (err) {
-        res.status(500).json(err)
-    }
+  try {
+    const updateUser = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, { new: true })
+    res.status(200).json(updateUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 // DELETE
-router.delete("/:id",verifyTokenAndAuthorization,async(req,res)=>{
-    try {
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("User has been deleted...")
-    } catch (err) {
-        res.status(500).json(err)
-    }
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User has been deleted...")
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 // GET
-router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
-    try {
-        const userFind = await User.findById(req.params.id)
-        const { password, ...others } = userFind._doc
-        res.status(200).json(others)
-    } catch (err) {
-        res.status(500).json(err)
-    }
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const userFind = await User.findById(req.params.id)
+    const { password, ...others } = userFind._doc
+    res.status(200).json(others)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 //GET USER STATS
 
 router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-    const date = new Date();
-    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-  
-    try {
-      const data = await User.aggregate([
-        { $match: { createdAt: { $gte: lastYear } } },
-        {
-          $project: {
-            month: { $month: "$createdAt" },
-          },
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
         },
-        {
-          $group: {
-            _id: "$month",
-            total: { $sum: 1 },
-          },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
         },
-      ]);
-      res.status(200).json(data)
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
 // GET ALL
-router.get("/",verifyTokenAndAdmin,async(req,res)=>{
-    const query = req.query.new
-    try {
-        const user = query ? await User.find().sort({_id: -1}).limit(5) : await User.find()
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new
+  try {
+    const user = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find()
 
-        res.status(200).json(user)
-    } catch (err) {
-        res.status(500).json(err)
-    }
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 module.exports = router
