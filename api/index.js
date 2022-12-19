a3956402331242c247b88be9c667a50f0168d66c
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-19 23:54:14 +0700

    CURD cart & order

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
+const stripeRoute = require('./routes/stripe')
+
 
 dotenv.config()
 
@@ -12,6 +16,9 @@ app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/stripe", stripeRoute)
 
 
 mongoose
