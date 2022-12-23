a4f8fa27d39c4c7e57a4eb9615ab9540dbbdf1d4
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-12-23 22:34:43 +0700

    admin : create pro #1

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,35 +1,61 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./newProduct.css";
 
 export default function NewProduct() {
+
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState([]);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleCat = (e) => {
+    setCat(e.target.value.split(","));
+  };
+
+  const handleClick =(e)=>{
+    
+  }
+
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input type="file" id="file" onChange={e=>setFile(e.target.files)[0]}/>
         </div>
         <div className="addProductItem">
           <label>Title</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <input name="title" type="text" placeholder="Skirts 2022" onChange={handleChange}/>
         </div>
         <div className="addProductItem">
           <label>Description</label>
-          <input type="text" placeholder="Description..." />
+          <input name="desc" type="text" placeholder="Description..." onChange={handleChange}/>
         </div>
         <div className="addProductItem">
           <label>Price</label>
-          <input type="text" placeholder="100" />
+          <input name="price" type="number" placeholder="100" onChange={handleChange}/>
+        </div>
+        <div className="addProductItem">
+          <label>Category</label>
+          <input type="text" placeholder="men, polo" onChange={handleCat}/>
         </div>
         <div className="addProductItem">
           <label>Stock</label>
-          <select name="inStock" >
+          <select name="inStock" onChange={handleChange}>
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
         </div>
         
-        <button className="addProductButton">Create</button>
+        <button onClick={handleClick} className="addProductButton">Create</button>
       </form>
     </div>
   );
