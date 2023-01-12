0f9ac8be569c3905b2d6a50646bb52696b6fd3ec
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2023-01-12 09:18:31 +0700

    client: register ->validate form

--- client/shopping/src/pages/Login-Register/Register.jsx
+++ client/shopping/src/pages/Login-Register/Register.jsx
@@ -13,7 +13,6 @@ const Register = () => {
   });
 
   const handleInputChange = (event) => {
-    /* event.persist(); NO LONGER USED IN v.17*/
     event.preventDefault();
 
     const { name, value } = event.target;
@@ -26,101 +25,136 @@ const Register = () => {
   const [submitted, setSubmitted] = useState(false);
   const [valid, setValid] = useState(false);
 
-  const validationForm = (e) => {
-    // let returnData = {
-    //   error: false,
-    //   msg: "",
-    // };
-    // //Kiểm tra email
-    // const re = /\S+@\S+\.\S+/;
-    // if (!re.test(email)) {
-    //   returnData = {
-    //     error: true,
-    //     msg: "Không đúng định dạng email",
-    //   };
-    // }
-    // //Kiểm tra password
-    // if (password.length < 6) {
-    //   returnData = {
-    //     error: true,
-    //     msg: "Mật khẩu phải lớn hơn 6 ký tự",
-    //   };
-    // }
-    // return returnData;
-  };
+  //const validationForm = (e) => {
+  //   let returnData = {
+  //     error: false,
+  //     msg: "",
+  //   };
+  //   //Kiểm tra email
+  //   const re = /\S+@\S+\.\S+/;
+  //   if (!re.test(email)) {
+  //     returnData = {
+  //       error: true,
+  //       msg: "Không đúng định dạng email",
+  //     };
+  //   }
+  //   //Kiểm tra password
+  //   if (password.length < 6) {
+  //     returnData = {
+  //       error: true,
+  //       msg: "Mật khẩu phải lớn hơn 6 ký tự",
+  //     };
+  //   }
+  //   return returnData;
+  // };
 
   const handleClick = (e) => {
     e.preventDefault();
-    const validation = validationForm(e);
-    if (validation.error) {
-      alert(validation.msg);
-    } else {
-      alert("Submit form success");
-    }
-    if (values.firstName && values.lastName && values.email) {
+    // const validation = validationForm(e);
+    // if (validation.error) {
+    //   alert(validation.msg);
+    // } else {
+    //   alert("Submit form success");
+    // }
+    if (
+      values.name &&
+      values.lastname &&
+      values.email &&
+      values.username &&
+      values.password &&
+      values.passwordConfirm
+    ) {
       setValid(true);
     }
     setSubmitted(true);
   };
+  console.log(values)
+  console.log('valid: ',valid);
   return (
     <div className="rg_container">
       <div className="rg_wrapper">
         <h1 className="rg_title">CREATE AN ACCOUNT</h1>
-        <form action="" className="rg_form">
+        <form action="" className="rg_form" onSubmit={handleClick}>
+        {submitted && valid && (
+          <div className="success-message">
+            <h3>
+              {" "}
+              Welcome {values.firstName} {values.lastName}{" "}
+            </h3>
+            <div> Your registration was successful! </div>
+          </div>
+        )}
           {!valid && (
             <input
               type="text"
+              name="name"
               placeholder="Name"
               className="rg_input"
               onChange={handleInputChange}
             />
           )}
+          {submitted && !values.name && <span id="rg_error">Please enter a first name</span>}
 
           {!valid && (
             <input
               type="text"
+              name="lastname"
               placeholder="Last name"
               className="rg_input"
               onChange={handleInputChange}
             />
           )}
+          {submitted && !values.lastname && <span id="rg_error">Please enter a lastname</span>}
+
           {!valid && (
             <input
               type="text"
+              name="username"
               placeholder="Username"
               className="rg_input"
               onChange={handleInputChange}
             />
           )}
+          {submitted && !values.username && <span id="rg_error">Please enter an username</span>}
+
           {!valid && (
             <input
-              type="text"
+              type="email"
+              name="email"
               placeholder="Email"
               className="rg_input"
               onChange={handleInputChange}
             />
           )}
+          {submitted && !values.email && <span id="rg_error">Please enter an email</span>}
+
           {!valid && (
             <input
-              type="text"
+              type="password"
+              name="password"
               placeholder="Password"
               className="rg_input"
               onChange={handleInputChange}
             />
           )}
+          {submitted && !values.password && <span id="rg_error">Please enter a password</span>}
+
           {!valid && (
             <input
-              type="text"
+              type="password"
+              name="passwordConfirm"
               placeholder="Confirm password"
               className="rg_input"
-              onChangonChange={handleInputChange}
+              onChange={handleInputChange}
             />
           )}
+          {submitted && !values.passwordConfirm && <span id="rg_error">Please enter password confirm</span>}
+
           <span className="rg_agreement">
             By creating an account, I consent to processing of my personal data in accordance with
             the <b>PRIVACY POLICY</b>
           </span>
-          <button className="rg_btn" onClick={handleClick}>
+          <button className="rg_btn" type="submit">
             CREATE
           </button>
         </form>
