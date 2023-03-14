e9320f48d48b3ab0d983a34552b367b9e9f265d1
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2023-03-14 17:20:21 +0700

    client-mobile: Get Quantity Product

--- client/mobile/Components/ProductCard/index.js
+++ client/mobile/Components/ProductCard/index.js
@@ -1,10 +1,19 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Button } from 'react-native'
 import React from 'react'
 
 import styles from './styles'
 
 const ProductCard = (props) => {
-  const { title, price, img } = props
+  const { title, price, img, size_color } = props
+  const k = Object.values(size_color) 
+  let totalPro =0
+  k.forEach(function(e){
+    let t = Object.values(e)
+    totalPro += t.reduce((total,current)=>{
+      return total+current
+    })
+    // console.log(t)
+  })
   return (
     <View style={styles.container}>
       <Image style={styles.image} />
@@ -13,6 +22,13 @@ const ProductCard = (props) => {
         {title.length > 15 ? title.substring(0, 12) + '...' : title}
       </Text>
       <Text style={styles.price}>${price}</Text>
+      {totalPro > 0 ?(
+        <View style={{marginBottom:60}}>
+          <Button title={'Add'} color={'green'}/>
+        </View>
+      ): 
+        <Text style={{marginTop:20}}>Currently Unvailable</Text>  
+      }
     </View>
   )
 }
