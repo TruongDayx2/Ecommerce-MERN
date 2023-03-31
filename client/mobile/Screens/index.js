1416a88c508d3da880747f7fb181c726a5b88770
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2023-03-31 11:23:38 +0700

    client:mob - bottom tabbar

--- client/mobile/Screens/index.js
+++ client/mobile/Screens/index.js
@@ -1,22 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text,Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react'
 import Home from './Home'
 import Cart from './Cart';
 import Products from './Products';
+import User from './User';
 
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator screenOptions={{headerShown:false}}>
+      <Tab.Screen name="Home" component={Home} options={{
+        tabBarIcon:({focused})=>(
+          <Image source={focused ? require('../assets/img/home2.png') : require('../assets/img/home1.png')} style={{width:30,height:30}} resizeMode="stretch" />
+        )
+      }}/>
+      <Tab.Screen name="Products" component={Products} />
+      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen name="User" component={User} />
+    </Tab.Navigator>
+  );
+}
 
 const Body = () => {
   return (
     // <Home/>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Cart" component={Cart} />
-        <Stack.Screen name="Products" component={Products} />
+      <Stack.Navigator initialRouteName="HomeTabs" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="HomeTabs" component={MyTabs} />
+       
       </Stack.Navigator>
     </NavigationContainer>
   )
