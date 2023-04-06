ccbf6f186ae17f24b265e5882712ad18212598b3
anhtn040 <anhhhh.0404@gmail.com>
2023-04-06 15:24:09 +0700

    add products

--- client/mobile/Components/ProductCard/index.js
+++ client/mobile/Components/ProductCard/index.js
@@ -1,39 +1,36 @@
-import { View, Text, Image, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Image, Button } from "react-native";
+import React from "react";
 
-import styles from './styles'
+import styles from "./styles";
 
 const ProductCard = (props) => {
-  const { title, price, img, size_color } = props
-  const k = Object.values(size_color) 
-  let totalPro =0
-  k.forEach(function(e){
-    let t = Object.values(e)
-    totalPro += t.reduce((total,current)=>{
-      return total+current
-    })
-  })
-  console.log(price)
+  const { title, price, img, size_color } = props;
+  const k = Object.values(size_color);
+  let totalPro = 0;
+  k.forEach(function (e) {
+    let t = Object.values(e);
+    totalPro += t.reduce((total, current) => {
+      return total + current;
+    });
+  });
+  console.log(price);
   return (
     <View style={styles.container}>
-      <Image style={styles.image} 
-        resizeMode='contain'
-        source={{uri:img?img : 'https://res.cloudinary.com/cloudinary-marketing/images/c_fill,w_895/f_auto,q_auto/v1649725549/Web_Assets/blog/loading-645268_1280/loading-645268_1280-jpg?_i=AA'}}
+      <Image
+        style={styles.image}
+        resizeMode="contain"
+        source={{
+          uri: img
+            ? img
+            : "https://res.cloudinary.com/cloudinary-marketing/images/c_fill,w_895/f_auto,q_auto/v1649725549/Web_Assets/blog/loading-645268_1280/loading-645268_1280-jpg?_i=AA",
+        }}
       />
-      <View style={styles.card} />
       <Text style={styles.title}>
-        {title.length > 15 ? title.substring(0, 12) + '...' : title}
+        {title.length > 15 ? title.substring(0, 12) + "..." : title}
       </Text>
-      <Text style={styles.price}>${price}</Text>
-      {totalPro > 0 ?(
-        <View style={{marginBottom:60}}>
-          <Button title={'Add'} color={'green'}/>
-        </View>
-      ): 
-        <Text style={{marginTop:20}}>Currently Unvailable</Text>  
-      }
+      <Text style={styles.price}>{price}VND</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
