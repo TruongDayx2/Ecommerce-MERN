0ac7e0675b92cfd9a95b7b624daba604a6652275
Trung Nguyen <trgnguyenfeb26@gmail.com>
2023-05-14 15:37:47 +0700

    fix port

--- api/index.js
+++ api/index.js
@@ -39,6 +39,6 @@ mongoose
     console.log(e);
   });
 
-app.listen(process.env.PORT || 5001, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
